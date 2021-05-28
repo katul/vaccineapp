2c5c5155ee31df57171c6d1b3526ac049d46c7bd
Atul Kumar <akguptah@gmail.com>
2021-05-28 21:14:26 +0530

    Added responsive error message

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,26 +34,28 @@ function fetchgo() {
     })
     .then((sessions)=>{
         if(sessions.length==0){
-            div.innerHTML+="<div class='error col-md-4'><div class='card p-3 mb-2'><div class='d-flex justify-content-between'><div class='d-flex flex-row align-items-center'><div class='ms-2 c-details'>Opps! No Slots available in your area.</div></div></div></div></div>"
+            div.innerHTML+="<div class='error col-md-4'><div class='error card p-3 mb-2'><div class='d-flex justify-content-between'><div class='d-flex flex-row align-items-center'><div class='ms-2 c-details'>Opps! No Slots available in your area.</div></div></div></div></div>"
         }else{
-            
-            
             for(let index=0; index < sessions.length; index++){
                 let dose1 = sessions[index].available_capacity_dose1;
                 let dose2 = sessions[index].available_capacity_dose2;
                 let dose = "";
-                if(dose1 == 0 && dose2 == 0){
-                    dose = "dosered";
+                if(dose1 == 0){
+                    colordose1 = "dosered";
                 }else{
-                    dose = "dosegreen"
+                    colordose1 = "dosegreen"
                 }
-                div.innerHTML+="<div class='dosered col-md-4'><div class='card p-3 mb-3'><div class='d-flex justify-content-between'><div class='d-flex flex-row align-items-center'><div class='icon'> <i class='bx bxl-mailchimp'></i> </div><div class='ms-2 c-details'><h6 class='mb-0'>"+sessions[index].vaccine+"</h6> <span>Age : "+sessions[index].min_age_limit+"+</span></div></div><div class='badge "+sessions[index].fee_type+"'> <span>"+sessions[index].fee_type+"</span> </div></div><div class='mt-1'><h3 class='heading "+dose+"'>Dose 1 Slots: "+sessions[index].available_capacity_dose1+"<br>Dose 2 Slots: "+sessions[index].available_capacity_dose2+"</h3><div class='mt-2'> <span class='text1'>"+sessions[index].name+" </span><p class='address'>"+sessions[index].address+"</p> </div></div></div></div>";
+                if(dose2 == 0){
+                    colordose2 = "dosered";
+                }else{
+                    colordose2 = "dosegreen"
+                }
+                div.innerHTML+="<div class='dosered col-md-4'><div class='card p-3 mb-3'><div class='d-flex justify-content-between'><div class='d-flex flex-row align-items-center'><div class='icon'> <i class='bx bxl-mailchimp'></i> </div><div class='ms-2 c-details'><h6 class='mb-0'>"+sessions[index].vaccine+"</h6> <span>Age : "+sessions[index].min_age_limit+"+</span></div></div><div class='badge "+sessions[index].fee_type+"'> <span>"+sessions[index].fee_type+"</span> </div></div><div class='mt-1'><h3 class='heading'><span class='"+colordose1+"'>Dose 1 Slots: "+sessions[index].available_capacity_dose1+"</span><br><span class='"+colordose2+"'>Dose 2 Slots: "+sessions[index].available_capacity_dose2+"</span></h3><div class='mt-2'> <span class='text1'>"+sessions[index].name+" </span><p class='address'>"+sessions[index].address+"</p> </div></div></div></div>";
             }
         }
     })
     .catch(function(error){
         console.log(error);
     })
-    //console.log(data.toString());
     return false;
 }
